edd216a8339fa0f37a6241cf71fbb2a916aaceaa
crussi <chrisrussi@gmail.com>
2017-05-31 21:51:11 -0700

    Added inbox-items

--- src/app/interfaces/app-state.interface.ts
+++ src/app/interfaces/app-state.interface.ts
@@ -1,6 +1,8 @@
 import {IPlatforms, IVideoGameListing} from '../video-games/interfaces';
+import {IInboxItemListing} from '../inbox-items/interfaces';
 
 export interface IAppState {
   readonly platforms: IPlatforms;
   readonly videoGameListing: IVideoGameListing;
+  readonly inboxItemListing: IInboxItemListing;
 }
