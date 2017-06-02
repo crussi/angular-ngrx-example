67e2b4bb01b94e0883f247225510d03e3f9aa5b6
crussi <chrisrussi@gmail.com>
2017-06-02 11:01:07 -0700

    Now only using inbox items

--- src/app/root/root.routes.ts
+++ src/app/root/root.routes.ts
@@ -3,6 +3,10 @@ import {Routes} from '@angular/router';
 import {videoGamesRoutes} from '../video-games/video-games.routes';
 import {inboxItemsRoutes} from '../inbox-items/inbox-items.routes';
 
+// export const rootRoutes: Routes = [
+//   ...videoGamesRoutes, ...inboxItemsRoutes
+// ];
+
 export const rootRoutes: Routes = [
-  ...videoGamesRoutes, ...inboxItemsRoutes
+  ...inboxItemsRoutes
 ];
